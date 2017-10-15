080bd4e160009e59e69ad1b942b6aa280fa8cff8
Rob Kovtiuk <robsonheater@gmail.com>
2017-10-15 14:11:39 +0300

    Build -> travis conf

--- src/test/kotlin/com/rkovtiuk/shortener/model/AbstractRepositoryTest.kt
+++ src/test/kotlin/com/rkovtiuk/shortener/model/AbstractRepositoryTest.kt
@@ -2,11 +2,14 @@ package com.rkovtiuk.shortener.model
 
 import com.github.springtestdbunit.DbUnitTestExecutionListener
 import com.rkovtiuk.shortener.ShortenerApplication
+import org.junit.runner.RunWith
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.test.annotation.DirtiesContext
 import org.springframework.test.context.TestExecutionListeners
 import org.springframework.test.context.TestPropertySource
 import org.springframework.test.context.junit4.AbstractTransactionalJUnit4SpringContextTests
+import org.springframework.test.context.junit4.SpringRunner
+import org.springframework.test.context.web.WebAppConfiguration
 
 /**
  * This class is developed by @author rkovtiuk on 21.09.17
@@ -16,7 +19,9 @@ import org.springframework.test.context.junit4.AbstractTransactionalJUnit4Spring
 
 @TestPropertySource(locations = arrayOf("classpath:repositories-test.properties"))
 @TestExecutionListeners(DbUnitTestExecutionListener::class)
+@RunWith(SpringRunner::class)
 @SpringBootTest(classes = arrayOf(ShortenerApplication::class))
+@WebAppConfiguration
 @DirtiesContext
 abstract class AbstractRepositoryTest : AbstractTransactionalJUnit4SpringContextTests() {
 
